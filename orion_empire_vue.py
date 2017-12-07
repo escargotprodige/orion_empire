a826164d9b6fef7d244097498862666040b67fbe
VargasOcando PaolaAndreina <1462618@decinfo.cvm>
2017-12-07 11:00:09 -0500

    buttons sarah

--- orion_empire_vue.py
+++ orion_empire_vue.py
@@ -481,7 +481,14 @@ class VueGalaxie(Perspective):
 		self.boutonShop.grid(row=2, column=0)
 
 		self.cadreSelectionVaisseau = Frame(self.cadreetat, bg="grey20")  # MODIF début
-# 
+		
+		self.cadreSelection = Frame(self.cadreetat, width=200, height=400, bg="blue")
+		self.cadreSelection.grid(row=10,column=0)
+		btnCharger = Button(self.cadreSelection, text="Charger Vaisseau", wraplength=80) #, command= ICI FONCTION SARAH)
+		btnDecharger = Button(self.cadreSelection, text="Decharger Vaisseau", wraplength=80) #, command= ICI FONCTION SARAH)
+		btnCharger.grid(row=0,column=0)
+		btnDecharger.grid(row=0,column=1)
+		
 #		 self.lbselectecible = Label(self.cadreSelectionVaisseau, text="Choisir cible", bg="darkgrey")
 #		 self.lbselectecible.grid(row=0, column=0)
 # 
@@ -1019,6 +1026,12 @@ class VueSysteme(Perspective):
 		self.boutonShop = Button(self.cadreetat, text="Shop ˃", command=self.afficherShop)
 		self.boutonShop.grid(row=1, column=0, sticky=W)
 		
+		self.cadreSelection = Frame(self.cadreetat, width=200, height=400, bg="blue")
+		self.cadreSelection.grid(row=10,column=0)
+		btnCharger = Button(self.cadreSelection, text="Charger Vaisseau", wraplength=80) #, command= ICI FONCTION SARAH)
+		btnDecharger = Button(self.cadreSelection, text="Decharger Vaisseau", wraplength=80) #, command= ICI FONCTION SARAH)
+		btnCharger.grid(row=0,column=0)
+		btnDecharger.grid(row=0,column=1)
 # 
 #	 def afficherShop(self):
 #		 self.boutonShop.config(text="Shop ˅")
@@ -1055,7 +1068,7 @@ class VueSysteme(Perspective):
 			self.cadreShop = None
 		else:
 			self.cadreShop = Frame(self.cadreetat, width=200, height=400, bg="blue")
-			self.cadreShop.grid(row=3, column=0, columnspan=5, rowspan=5)
+			self.cadreShop.grid(row=3, column=0)
 			shopVTransport = Button(self.cadreShop, text="Vaisseau Transport", wraplength=80, image=self.images["shopTransport"], compound="top", command=self.shopTransport)
 			shopVTransport.grid(row=0, column=0)
 			
@@ -1082,10 +1095,10 @@ class VueSysteme(Perspective):
 		else:
 			pass
 		self.cadreInfoShop = Frame(self.cadreShop, width=200, height=100)
-		self.cadreInfoShop.grid(row=3, column=0, columnspan=5, rowspan=5)
+		self.cadreInfoShop.grid(row=1, column=0, columnspan=5, rowspan=5)
 		# Infos batiment
 		labelImage = Label(self.cadreInfoShop, image=self.images["shopTransport"])
-		labelNom = Label(self.cadreInfoShop, text="Vaisseau Transport",wraplength=90,)
+		labelNom = Label(self.cadreInfoShop, text="Vaisseau Transport",wraplength=90)
 		labelLvl = Label(self.cadreInfoShop, text="Lvl. 1")
 		# Cout batiment
 		label = Label(self.cadreInfoShop, text="Cout")
@@ -1586,6 +1599,8 @@ class VuePlanete(Perspective):
 
 		self.boutonSelect = Button(self.cadreetat, text="Selection >", command=self.afficherSelection)
 		self.boutonSelect.grid(row=10, column=0, sticky=W)
+		
+		
 
 	def afficherShop(self):
 		self.boutonShop.config(text="Shop ˅")
@@ -1621,6 +1636,7 @@ class VuePlanete(Perspective):
 								 command=self.infoBarrack)
 			shopBarrack.grid(row=1, column=1)
 
+		
 #			 # À EFFACER, TEMPORAIRE
 #			 shopLazerboi = Button(self.cadreShop, text="Lazerboi", image=self.images["lazerboi"], compound="top",
 #								   command=self.infoLazerboi)
@@ -1711,6 +1727,8 @@ class VuePlanete(Perspective):
 		# bouton
 		boutonAcheter.grid(row=4, column=4)
 
+
+	
 	def infoVille(self):
 		self.macommande = None
 		self.infoShop("ville")
@@ -2015,6 +2033,8 @@ class VuePlanete(Perspective):
 			label.grid(row=2, column=2)
 			label = Label(self.cadreSelection, text="Y " + str(self.moveY))
 			label.grid(row=3, column=2)
+			btnCharger = Button(self.cadreSelection, text="Charger Units", wraplength=80)
+			btnCharger.grid(row=4,column=4)
 				
 			
 	def shopSelection(self, type):
